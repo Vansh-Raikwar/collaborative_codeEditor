9cc0d720f1b51d1966796e5229de8d4fa67016da
Vansh <143195512+Vansh-Raikwar@users.noreply.github.com>
2026-05-29 00:26:35 +0530

    change the colour of join button

--- client/src/components/forms/FormComponent.jsx
+++ client/src/components/forms/FormComponent.jsx
@@ -111,7 +111,7 @@ const FormComponent = () => {
                 />
                 <button
                     type="submit"
-                    className="mt-2 w-full rounded-md bg-primary px-8 py-3 text-lg font-semibold text-black"
+                    className="mt-2 w-full rounded-md bg-primary px-8 py-3 text-lg font-semibold text-black bg-[#7d61e1]"
                 >
                     Join
                 </button>
